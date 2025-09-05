f698ed3aeeaa8e56cf27aae66679e236d267b75c
sujit-27 <sujitshaw029@gmail.com>
2025-09-05 16:37:42 +0530

    Prepare frontend for vercel deployment

--- FilePulse_Backend/src/main/java/com/myproject/FilePulse/config/SecurityConfig.java
+++ FilePulse_Backend/src/main/java/com/myproject/FilePulse/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://filepulse.vercel.app","https://filepulse-git-main-sujti-kumar-shaws-projects.vercel.app"));
+        config.setAllowedOrigins(List.of("https://filepulse.vercel.app","https://filepulse-git-main-sujti-kumar-shaws-projects.vercel.app","https://filepulse-mk0gcqhzm-sujti-kumar-shaws-projects.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
