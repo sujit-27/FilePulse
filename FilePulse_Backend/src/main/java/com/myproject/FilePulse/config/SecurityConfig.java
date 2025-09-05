3c75f0685989e8641f4d096e27a625b5a337c99e
sujit-27 <sujitshaw029@gmail.com>
2025-09-05 15:19:07 +0530

    Prepare frontend for vercel deployment

--- FilePulse_Backend/src/main/java/com/myproject/FilePulse/config/SecurityConfig.java
+++ FilePulse_Backend/src/main/java/com/myproject/FilePulse/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
 
     private UrlBasedCorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-         config.setAllowedOrigins((List.of("http://localhost:5173")));
+         config.setAllowedOrigins((List.of("https://filepulse.onrender.com")));
          config.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
          config.setAllowedHeaders(List.of("Authorization" , "Content-Type"));
          config.setAllowCredentials(true);
